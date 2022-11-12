b69adbc38692d6e14def754afd3f152334fc87d9
Nicholas Lever <nik.j.lever@gmail.com>
2022-11-12 12:11:04 +0000

    block example

--- complete/blocks/js/blockHandler.js
+++ complete/blocks/js/blockHandler.js
@@ -1,13 +1,44 @@
 WL.registerComponent('blockHandler', {
-    param: {type: WL.Type.Float, default: 1.0},
+    vrCamera: {type: WL.Type.Object, default: null},
+    speed: {type: WL.Type.Float, default: 5.0 }
 }, {
     init: function() {
-        console.log('init() with param', this.param);
+        this.rotation = glMatrix.quat.create();
+        glMatrix.quat.fromEuler(this.rotation, 1, 1, 0);
+        this.tmpQuat = glMatrix.quat.create();
+        this.block = this.object.children[0];
+        this.direction = glMatrix.vec3.create();
+        this.tmpVec = glMatrix.vec3.create();
+        this.tmpVec1 = glMatrix.vec3.create();
     },
     start: function() {
-        console.log('start() with param', this.param);
+        this.spawn();
+    },
+    spawn: function() {
+        if ( this.vrCamera == null ) return;
+        this.vrCamera.getForward( this.direction );
+        glMatrix.vec3.copy( this.tmpVec, this.direction );
+        glMatrix.vec3.scale( this.tmpVec, this.tmpVec, 30 );
+        this.vrCamera.getTranslationWorld( this.tmpVec1 );
+        glMatrix.vec3.add( this.tmpVec, this.tmpVec, this.tmpVec1 );
+        this.object.setTranslationWorld( this.tmpVec );
+        glMatrix.vec3.negate( this.direction, this.direction );
+        glMatrix.vec3.scale( this.direction, this.direction, this.speed );
     },
     update: function(dt) {
-        console.log('update() with delta time', dt);
+        glMatrix.quat.scale( this.tmpQuat, this.rotation, dt );
+        this.block.rotateObject( this.tmpQuat );
+        if ( this.vrCamera != null ){
+            glMatrix.vec3.copy( this.tmpVec, this.direction );
+            glMatrix.vec3.scale( this.tmpVec, this.tmpVec, dt );
+            this.object.translate( this.tmpVec );
+            this.object.getTranslationWorld( this.tmpVec );
+            this.vrCamera.getTranslationWorld( this.tmpVec1 );
+            glMatrix.vec3.subtract( this.tmpVec, this.tmpVec, this.tmpVec1 );
+            glMatrix.vec3.normalize( this.tmpVec, this.tmpVec );
+            this.vrCamera.getForward( this.tmpVec1 );
+            const theta = glMatrix.vec3.angle( this.tmpVec, this.tmpVec1 );
+            if (theta > Math.PI/2 ) this.spawn();
+        }
     },
 });
