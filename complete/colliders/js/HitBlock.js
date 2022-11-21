68967efda7bba6c7ae3e5e0965a706b0a69194ae
Nicholas Lever <nik@catalystpics.co.uk>
2022-11-21 12:44:57 +0000

    Added handedness to Collliders>blockHandler

--- complete/colliders/js/HitBlock.js
+++ complete/colliders/js/HitBlock.js
@@ -1,35 +1,34 @@
 WL.registerComponent('HitBlock', {
-    //controller: { type: WL.Type.Object },
     collisionIndicator: { type: WL.Type.Object }, 
     colliderGroup: { type: WL.Type.Int, default: 6 },
-    //handedness: {type: WL.Type.Enum, values: ['input component', 'left', 'right', 'none'], default: 'input component'}
+    handedness: {type: WL.Type.Enum, values: ['input component', 'left', 'right', 'none'], default: 'input component'}
 }, {
     init: function() {    
         this.tmpVec = new Float32Array(3);
         this.tmpVec1 = new Float32Array(3);
     },
     start: function() {
-        /*this.input = this.object.getComponent('input');
-        if(!this.input) {
-            console.error(this.object.name, "collectable.js: input component is required on the object");
+        const input = this.object.getComponent('input');
+        if(!input) {
+            console.error(this.object.name, "HitBlock.js: input component is required on the object");
             return;
         }else if(this.handedness == 0){
-            this.handedness = this.input.handedness;
+            this.handedness = input.handedness;
+        }else{
+            this.handedness = ['left', 'right'][this.handedness-1];
         }
-        this.cursor = this.object.getComponent('cursor'); */
+        this.cursor = this.object.getComponent('cursor');
         WL.onXRSessionStart.push(this.setupVREvents.bind(this));
     },
     setupVREvents: function(s){
         this.session = s;
         s.addEventListener('end', function() {
-            /* Reset cache once the session ends to rebind select etc, in case
-             * it starts again */
             this.session = null;
         }.bind(this));
 
         s.addEventListener('selectstart', (e) => {
             //console.log(`HitBlock select ${this.handedness}`);
-            if(this.hitData) {
+            if(this.hitData && e.inputSource.handedness == this.handedness) {
                 const blockHandler = this.hitData.object.getComponent('blockHandler');
                 if (blockHandler) blockHandler.hitBlock();
             }
