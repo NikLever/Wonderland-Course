ae0bbddecc5d07ffe0ec7443c5e3f8a4f27d29c3
Nicholas Lever <nik.j.lever@gmail.com>
2022-12-08 10:20:57 +0000

    Repair Ghouls

--- complete/ghouls/js/BulletHandler.js
+++ complete/ghouls/js/BulletHandler.js
@@ -16,12 +16,13 @@ WL.registerComponent('BulletHandler', {
     },
     update: function(dt) {
         glMatrix.vec3.copy( this.tmpVec, this.direction );
-        glMatrix.vec3.scale( this.tmpVec, this.tmpVec, dt * 1 );
+        glMatrix.vec3.scale( this.tmpVec, this.tmpVec, dt * 10 );
         this.object.translate( this.tmpVec );
         this.elapsedTime += dt;
         if ( this.elapsedTime > 3) this.object.active = false;
         const overlaps = this.collision.queryOverlaps();
         if (overlaps.length > 0){
+            console.log('BulletHandler.update: overlap');
             const ghoulHandler = overlaps[0].object.parent.getComponent( 'GhoulHandler' );
             if (ghoulHandler) ghoulHandler.shot();
             this.object.active = false;
