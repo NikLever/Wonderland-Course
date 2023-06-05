cc95569f265ad020993ffb15048d05577dae56e3
NikLever <nik.j.lever@gmail.com>
2023-06-05 17:03:57 +0100

    Factory project updated

--- complete/factory/js/MovePlayer.js
+++ complete/factory/js/MovePlayer.js
@@ -1,27 +1,34 @@
-WL.registerComponent('MovePlayer', {
-    //navmesh: {type: WL.Type.Object, default: null},
-    camera: { type: WL.Type.Object },
-    speed: { type: WL.Type.Float, default: 2.0 }
-}, {
-    init: function() {
+import {Component, Property} from '@wonderlandengine/api';
+import { vec3 } from "gl-matrix";
+
+export class Name extends Component {
+    static TypeName = "MovePlayer";
+    static Properties = { 
+        camera: Property.object(),
+        speed: Property.float( 2.0 )
+    };
+    
+    init() {
         this.tmpPos = new Float32Array(3);
         this.tmpVec = new Float32Array(3);
         this.down = new Float32Array(3);
         this.down.set([0,-1,0]);
         this.selectPressed = { left: false, right: false };
-    },
-    start: function() {
+    }
+
+    setTranslationWorld() {
         //this.physx = (this.navmesh) ? this.navmesh.getComponent('physx') : null;
-        WL.onXRSessionStart.push(this.setupVREvents.bind(this));
-    },
-    update: function(dt) {
+        this.engine.onXRSessionStart.push(this.setupVREvents.bind(this));
+    }
+
+    update(dt) {
         if ( this.camera == null ) return;
         if (this.getSelectPressed()){
             this.object.getTranslationWorld( this.tmpPos );
             this.camera.getForward( this.tmpVec );
 
-            glMatrix.vec3.scale(this.tmpVec, this.tmpVec, dt * this.speed);
-            glMatrix.vec3.add( this.tmpPos, this.tmpPos, this.tmpVec);
+            vec3.scale(this.tmpVec, this.tmpVec, dt * this.speed);
+            vec3.add( this.tmpPos, this.tmpPos, this.tmpVec);
             
             this.tmpPos[1] += 1;
             
@@ -31,18 +38,22 @@ WL.registerComponent('MovePlayer', {
                 this.object.setTranslationWorld(rayhit.locations[0]);
             }
         }
-    },
-    setupVREvents: function(s){
+    }
+
+    setupVREvents(s){
     	s.addEventListener('selectstart', this.selectStart.bind(this));
         s.addEventListener('selectend', this.selectEnd.bind(this));
-    },
-    selectStart: function(e){
+    }
+
+    selectStart(e){
     	this.selectPressed[e.inputSource.handedness] = true;	
-    },
-    selectEnd: function(e){
+    }
+
+    selectEnd(e){
     	this.selectPressed[e.inputSource.handedness] = false;
-    },
-    getSelectPressed: function(){
+    }
+
+    getSelectPressed(){
     	return this.selectPressed.left || this.selectPressed.right;
     }
-});
+}
